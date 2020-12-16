3791a7a770d4ddbe3d002131a0bb3f4e68bd2030
Nicolas Eckhart <nicolas.eckhart@localsearch.ch>
2020-12-16 13:10:22 +0100

    Add GraphQL Hooks example

---
+++ helpers/graphql-hooks-client.ts
@@ -0,0 +1,9 @@
+import { GraphQLClient } from 'graphql-hooks'
+import memCache from 'graphql-hooks-memcache'
+
+export const getClient = () => (
+  new GraphQLClient({
+    url: 'http://localhost:8080/v1/graphql',
+    cache: memCache()
+  })
+)
